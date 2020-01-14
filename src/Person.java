b8936a290872825df80503650595ee91d2f6d3ee
Marek Wiśniowski <mwisniowski@o2.pl>
2020-01-14 22:19:22 +0100

    Added .gitignore and other

---
+++ src/Person.java
@@ -0,0 +1,35 @@
+public abstract class Person {
+    private String firstName;
+    private String secondName;
+    private int age;
+
+    public Person(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
